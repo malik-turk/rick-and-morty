9e19575ae2ff815cb58d7a48e1aef348026216ff
ngTurk <malik_alturk@hotmail.com>
2022-11-29 21:12:41 +0200

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { useQuery } from "@apollo/client";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Divider from '@mui/material/Divider';
+import { styled } from "@mui/material/styles";
 
 // Queries
 import { CHARACTERS_QUERY } from "./graphql/characters-query";
+
+// Styled components
+const Title = styled(Typography)(({ theme }) => ({
+  marginTop: "12px",
+}));
 
 function App() {
   const { loading, error, data } = useQuery(CHARACTERS_QUERY);
 
   return (
-    <div></div>
+    <Container maxWidth="lg">
+      <header>
+        <Title variant="h4" gutterBottom>
+          Rick And Morty Characters
+        </Title>
+        <Divider />
+      </header>
+    </Container>
   );
 }
 
